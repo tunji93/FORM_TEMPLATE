ac2b8a8b0ec464287646e637dc411bbd72fa734f
wahab <aderintoadetunji@gmail.com>
2022-02-08 20:14:31 +0100

    completed formcard markup

--- src/components/form_card/formCard.js
+++ src/components/form_card/formCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import "./_formCard.scss"
+import "../../styles/_formCard.scss"
 
-const formCard = () => {
-  return <div>card template</div>;
+const FormCard = () => {
+  return (
+    <div className = "card">
+      <div className = "card__main">
+        <h3>Alumni Membership Form Template</h3>
+        <p>engage your alumni network better with this alumni registration form template embeded in your website ...</p>
+      </div>
+      <div className = "card__bottom">
+        <h3>use template</h3>
+      </div>
+    </div>
+  )
+    
+  
 };
 
-export default formCard;
+export default FormCard;
 
